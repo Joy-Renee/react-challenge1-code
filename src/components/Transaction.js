4488fa2ba801ea13b6cf7f7e54e370006f3d79b3
Joy Renee Mugo <mugojoy24@gmail.com>
2024-04-28 15:58:45 +0300

    commit

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -10,9 +10,30 @@ function Addtransaction() {
         amount: ""
     })
 
+    const [transactions, setTransactions] = useState([]);
+
     function handleSubmit(event) {
         event.preventDefault()
         console.log(formTransact);
+        const newTransaction = {
+            date: formTransact.date,
+            description: formTransact.description,
+            category: formTransact.category,
+            amount: formTransact.amount
+        };
+    
+
+    // Add the new transaction to the list of transactions
+    setTransactions([...transactions, newTransaction]);
+    // Clear the form after submission
+    addTransact({
+        ...formTransact,
+        date: "",
+        description: "",
+        category: "",
+        amount: ""
+    });
+
     }
 
     function handleChange(event) {
@@ -28,67 +49,91 @@ function Addtransaction() {
     console.log(addTransact);
 
     return(
-        <div>
-            <form onSubmit={handleSubmit}>
+        <div id="div">
+            <form onSubmit={handleSubmit} id="submited">
                 <h1 id="header">The Royal Bank of Flatiron</h1>
-                <input 
-                type="text"
-                id="search"
-                className="added"
-                value={formTransact.search}
-                placeholder="Search your Recent Transaction"
-                onChange={handleChange}
-                />
+                <div id="search">
+                    <input 
+                    type="text"
+                    id="search"
+                    className=""
+                    value={formTransact.search}
+                    placeholder="Search your Recent Transaction"
+                    onChange={handleChange}
+                    />
+                </div>
 
-                <input 
-                type="date"
-                id="date"
-                className="added"
-                value={formTransact.date}
-                placeholder=""
-                onChange={handleChange}
-                /> 
-
-                <input 
-                type="text"
-                id="description"
-                className="added"
-                value={formTransact.description}
-                placeholder="description"
-                onChange={handleChange}
-                />
-
-                <input 
-                type="text"
-                id="category"
-                className="added"
-                value={formTransact.category}
-                placeholder="category"
-                onChange={handleChange}
-                />
-
-                <input 
-                type="number"
-                id="amount"
-                className="added"
-                value={formTransact.amount}
-                placeholder="Amount"
-                onChange={handleChange}
-                />
                 <br></br>
-                <br></br>
-               <div>
-               <input 
-                type="submit" 
-                id="submitt"
-                className="submitt"
-                value="Add Transaction" 
                 
-                />
-               </div>
+                <div>
+                    <input 
+                    type="date"
+                    id="date"
+                    className="added"
+                    value={formTransact.date}
+                    placeholder=""
+                    onChange={handleChange}
+                    /> 
+
+                    <input 
+                    type="text"
+                    id="description"
+                    className="added"
+                    value={formTransact.description}
+                    placeholder="description"
+                    onChange={handleChange}
+                    />
+
+                    <input 
+                    type="text"
+                    id="category"
+                    className="added"
+                    value={formTransact.category}
+                    placeholder="category"
+                    onChange={handleChange}
+                    />
+
+                    <input 
+                    type="number"
+                    id="amount"
+                    className="added"
+                    value={formTransact.amount}
+                    placeholder="Amount"
+                    onChange={handleChange}
+                    />
+                    <br></br>
+                    <br></br>
+                    <div>
+                        <input 
+                            type="submit" 
+                            id="submitt"
+                            className="submitt"
+                            value="Add Transaction" 
+                            
+                            />
+                    </div>
+                </div>
+
             </form>
+
+        <table>
+            <div>
+               
+                    {transactions.map((transaction, index) => (
+                        <div key={index}>
+                            <div>Date: {transaction.date}</div>
+                            <div>Description: {transaction.description}</div>
+                            <div>Category: {transaction.category}</div>
+                            <div>Amount: {transaction.amount}</div>
+                        </div>
+                    ))}
+                
+            </div>
+        </table>
         </div>
     )
+
+
     
 }
 
